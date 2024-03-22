34f2e81d61a4e7b7bfdab114877195ba79f08b6c
sangwook <bnbt3@naver.com>
2024-03-22 15:29:05 +0900

    섹션2 마무리

---
+++ spring_basic/src/main/java/com/spring/lab/spring_basic/discount/FIxDiscountPolicy.java
@@ -0,0 +1,16 @@
+package com.spring.lab.spring_basic.discount;
+
+import com.spring.lab.spring_basic.member.Grade;
+import com.spring.lab.spring_basic.member.Member;
+
+public class FIxDiscountPolicy implements DiscountPolicy {
+    private final int discountFixAmount = 1000; // 1000원 할인
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return discountFixAmount;
+        }
+        return 0;
+    }
+}
