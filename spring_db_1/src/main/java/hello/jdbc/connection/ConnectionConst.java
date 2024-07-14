35dbea438b129136abefcce1128008261d2a253c
sangwook <bnbt3@naver.com>
2024-07-14 13:11:49 +0900

    section1

---
+++ spring_db_1/src/main/java/hello/jdbc/connection/ConnectionConst.java
@@ -0,0 +1,8 @@
+package hello.jdbc.connection;
+
+public abstract class ConnectionConst {
+
+  public static final String URL = "jdbc:h2:tcp://localhost:9092/~/repository/backend_study/spring_db_1/spring_db_1";
+  public static final String USER = "sa";
+  public static final String PASSWORD = "";
+}
